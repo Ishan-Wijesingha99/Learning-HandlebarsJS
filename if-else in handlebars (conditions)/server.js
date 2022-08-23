b9a2f8905c71be0a5b5c2fd5622178c654796559
Ishan <ishanwijes@gmail.com>
2022-08-23 19:08:26 +1000

    if statements completed

---
+++ if-else in handlebars (conditions)/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+
+
+// need this to set up handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+app.use(express.static('./public'))
+
+
+
+app.get('/', (req, res) => {
+    // let's use two different variables for the two different booleans
+    res.render('list', {layout: 'main', thisIsTrue: true, thisIsFalse: false})
+})
+
+
+
+app.listen(5000, () => {
+    console.log('app is listening on port 5000...')
+})
