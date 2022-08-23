6266f1441bbdb553b5ddc24b4bc93e16d3765842
Ishan <ishanwijes@gmail.com>
2022-08-23 16:36:32 +1000

    partials completed

--- partials/server.js
+++ partials/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+
+
 // need this to set up handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -12,13 +14,14 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('./public'))
 
+
+
 app.get('/', (req, res) => {
-    // when sending html files with handlebars, instead of send(), use render()
-    // render() takes two parameters 
-    // the first should be 'main' so that it points to main.handlebars
-    // the second is an object with a layout property which points to the index.handlebars file
+    // list.handlebars will be rendered as {{{body}}} in main.handlebars
     res.render('list', {layout: 'main'})
 })
+
+
 
 app.listen(5000, () => {
     console.log('app is listening on port 5000...')
